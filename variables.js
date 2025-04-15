5ce10040ef4550f3c74a85a83dd887c716ccfef1
Wisp <wisp3abyss@gmail.com>
2025-04-15 12:42:43 -0400

    v1 upload

---
+++ variables.js
@@ -0,0 +1,285 @@
+// variables.js
+const iconMap = {
+  travis: {
+      image: "img/icons/Travis.webp",
+      links: {
+          visit: "https://www.youtube.com/@artemiykopych",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Travis",
+          desktop: "https://www.mediafire.com/file/idei7d0mqg7p5pe/Incredibox_-_The_Story_of_Travis_v1.3.0.zip/file",
+          mobile: "https://www.mediafire.com/file/5n93s3babvv26n2/Incredibox_-_The_Story_of_Travis_v1.3.0.apk/file"
+      }
+  },
+  evadare: {
+      image: "img/icons/Evadare.webp",
+      links: {
+          visit: "https://www.incredirem.com/",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Evadare",
+          desktop: "https://www.mediafire.com/file/qyi5esxp8r2oj0g/Evadare_v0.5.0.zip/file",
+          mobile: "https://www.mediafire.com/file/0pcsbyre2gameir/Evadare_0.5.0.apk/file"
+      }
+  },
+  xrun: {
+      image: "img/icons/Xrun.webp",
+      links: {
+          visit: "https://www.incredirem.com/",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Xrun",
+          desktop: "https://www.mediafire.com/file/qyi5esxp8r2oj0g/Evadare_v0.5.0.zip/file",
+          mobile: "https://www.mediafire.com/file/0pcsbyre2gameir/Evadare_0.5.0.apk/file"
+      }
+  },
+  void: {
+    image: "img/icons/Void.webp",
+    links: {
+        visit: "https://www.incredirem.com/",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Void",
+        desktop: "https://www.mediafire.com/file/qyi5esxp8r2oj0g/Evadare_v0.5.0.zip/file",
+        mobile: "https://www.mediafire.com/file/0pcsbyre2gameir/Evadare_0.5.0.apk/file"
+    }
+  },
+  fsp: {
+    image: "img/icons/FSP.webp",
+    aliases: ["full stop punctuation"],
+    links: {
+        visit: "https://www.incredirem.com/",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Void",
+        desktop: "https://www.mediafire.com/file/qyi5esxp8r2oj0g/Evadare_v0.5.0.zip/file",
+        mobile: "https://www.mediafire.com/file/0pcsbyre2gameir/Evadare_0.5.0.apk/file"
+    }
+},
+  downtown: {
+      image: "img/icons/Downtown.webp",
+      links: {
+          visit: "https://www.youtube.com/@PersonFromBrazil",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Downtown",
+          desktop: "https://www.mediafire.com/file/syez2ktc093rnb9/PFBs_Workshop_v1.0.0_%2528PC%2529.zip/file",
+          mobile: "https://www.mediafire.com/file/1gecnl49rum57dc/PFBs_Workshop_v1.0.0_%2528Android%2529.apk/file"
+      }
+  },
+  mechanic: {
+      image: "img/icons/Mechanic.webp",
+      aliases: ["coldbox"],
+      links: {
+          visit: "https://www.youtube.com/@coldseamer_icecube",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Mechanic",
+          desktop: "https://www.mediafire.com/file/kst2v5sdfcom8q9/Coldbox+-+PC+Port.zip/file",
+          mobile: "https://www.mediafire.com/file/0j86glp0wtja8ig/Coldbox_fixed.apk/file"
+      }
+  },
+  bonfire: {
+      image: "img/icons/Bonfire.webp",
+      aliases: ["coldbox"],
+      links: {
+          visit: "https://www.youtube.com/@coldseamer_icecube",
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Bonfire",
+          desktop: "https://www.mediafire.com/file/kst2v5sdfcom8q9/Coldbox+-+PC+Port.zip/file",
+          mobile: "https://www.mediafire.com/file/0j86glp0wtja8ig/Coldbox_fixed.apk/file"
+      }
+  },
+  dreamland: {
+      image: "img/icons/Dreamland.webp",
+      links: {
+          wiki: "https://incredibox.fandom.com/wiki/Fanon:Dreamland",
+          desktop: "https://www.mediafire.com/file/mdr5bx9dopdoth5/Dreamland.zip/file",
+          mobile: "https://www.mediafire.com/file/09622ol7a8w0qc8/Incredibox_-_Dreamland.apk/file"
+      }
+  },
+  airline: {
+    image: "img/icons/Airline.webp",
+    aliases: ["arbox"],
+    links: {
+        visit: "https://sites.google.com/view/rb-website/arbox",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Airline",
+        mobile: "https://www.mediafire.com/file/8n60sw5e2ntplw9/Arbox+-+Episode+4+(+Full+).apk/file"
+    }
+  },
+  augury: {
+    image: "img/icons/Augury.webp",
+    aliases: ["arbox"],
+    links: {
+        visit: "https://sites.google.com/view/rb-website/arbox",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Augury",
+        mobile: "https://www.mediafire.com/file/8n60sw5e2ntplw9/Arbox+-+Episode+4+(+Full+).apk/file"
+    }
+  },
+  armed: {
+    image: "img/icons/Armed.webp",
+    aliases: ["arbox"],
+    links: {
+        visit: "https://sites.google.com/view/rb-website/arbox",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Armed",
+        mobile: "https://www.mediafire.com/file/8n60sw5e2ntplw9/Arbox+-+Episode+4+(+Full+).apk/file"
+    }
+  },
+  beatnik: {
+    image: "img/icons/Beatnik.png",
+    links: {
+        visit: "https://boogoxseal.xyz/",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Beatnik",
+        desktop: "https://www.mediafire.com/file/ykxaqrsjc0hl449/Beatnik-windows.zip/file",
+        mobile: "https://www.mediafire.com/file/6b1yqmwgzdkli2h/Beatnik-android.apk/file"
+    }
+  },
+  autumn: {
+    image: "img/icons/Autumn.png",
+    links: {
+        visit: "https://www.youtube.com/@oCharlles",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:The Seasons/Autumn",
+        desktop: "https://www.mediafire.com/file/1i5loxvqwt53ihp/The_Seasons_-_Autumn_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/21kfz4w5q2ab4t5/The_Seasons_-_Autumn.apk/file"
+    }
+  },
+  skylines: {
+    image: "img/icons/Skylines.webp",
+    aliases: ['Trilly Tunes'],
+    links: {
+        visit: "https://www.youtube.com/@Trillygot",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Skylines",
+        desktop: "https://www.mediafire.com/file/6y2rrw1ouoilu82/Trillys_Tunes_v1.1_-_Skylines_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/5ii0gx0hnwe9eqa/Trilly%2527s_Tunes_v1.1_-_Skylines.apk/file"
+    }
+  },
+  abloom: {
+    image: "img/icons/Abloom.webp",
+    aliases: ['Trilly Tunes'],
+    links: {
+        visit: "https://www.youtube.com/@Trillygot",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Abloom",
+        desktop: "https://www.mediafire.com/file/71jeeio35f1kmlr/Trillys_Tunes_v2.0_-_Abloom_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/fogczf106ta1yzh/Trilly%2527s_Tunes_v2.0_-_Abloom.apk/file"
+    }
+  },
+  bells: {
+    image: "img/icons/Bells.webp",
+    aliases: ['The Bells'],
+    links: {
+        visit: "https://www.incredirem.com/",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:The Bells",
+        desktop: "https://www.mediafire.com/file/hfctc5x8rxdzj0m/The+Bells+3.0+-+PC.zip",
+        mobile: "https://www.mediafire.com/file/v8wzmkmt1gbu47g/Incredibox_-_The_Bells_v3.0.apk"
+    }
+  },
+  geek: {
+    image: "img/icons/Geek.webp",
+    aliases: ['Geek Dance'],
+    links: {
+        visit: "https://www.youtube.com/@GegsPunchLine",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Geek_Dance",
+        desktop: "https://www.mediafire.com/file/jkkmcg1obvucmup/Geek_Dance_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/cgg28bkefi6rzdn/Geek_Dance_-_V_1.0.apk/file"
+    }
+  },
+  pecadoras: {
+    image: "img/icons/Pecadoras.png",
+    links: {
+        visit: "https://www.youtube.com/@collegenollegeyt",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Pecadoras",
+        desktop: "https://www.mediafire.com/file/rc46big0gcrdu0q/Pecadoras+(Windows).zip/file",
+        mobile: "https://www.mediafire.com/file/z0byin98t1u9x2s/Pecadoras_%28Android%29.apk/file"
+    }
+  },
+  burn: {
+    image: "img/icons/Burn.webp",
+    links: {
+        visit: "https://www.youtube.com/@dead-eye1464",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Burn",
+        desktop: "https://www.mediafire.com/file/50vma0avppq9b8u/GroovBox.zip/file",
+        mobile: "https://www.mediafire.com/file/yc5jlg3s6fwqu9f/Groovbox__%28Android%29.apk/file"
+    }
+  },
+  celcius: {
+    image: "img/icons/Celcius.webp",
+    links: {
+        visit: "https://www.youtube.com/@Limezic",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Celcius",
+        desktop: "https://www.mediafire.com/file/1qhyo9ycas83yzr/Project_C%25C2%25B0_-_Celcius.zip/file",
+        mobile: "https://www.mediafire.com/file/dicrsmdd88vb89h/Project_C%25C2%25B0_-_Celcius_%2528Android%2529.apk/file"
+    }
+  },
+  crime: {
+    image: "img/icons/Crime.webp",
+    aliases: ['The Crime'],
+    links: {
+        visit: "https://www.youtube.com/@R.K-z2d",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:The Crime",
+        desktop: "https://www.mediafire.com/file/16ceizye79yjnke/Incredibox_-_The_Crime_v1.0.1_%2528PC%2529.zip/file",
+        mobile: "https://www.mediafire.com/file/gin6w4futbrjb9r/Incredibox+-+The+Crime_signed.apk/file"
+    }
+  },
+  express: {
+    image: "img/icons/Express.webp",
+    links: {
+        visit: "https://www.youtube.com/@splat-jack",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Express",
+        desktop: "https://www.mediafire.com/file/25ay2wtyije8xbt/EXPRESS_PC_PORT.zip/file",
+    }
+  },
+  tld: {
+    image: "img/icons/TLD.png",
+    aliases: ['The Last Day', 'The Unreleased'],
+    links: {
+        visit: "https://www.youtube.com/@PersonFromBrazil",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:The_Unreleased_V1",
+        desktop: "https://www.mediafire.com/file/lukkfuqfmcrnl2w/Incredibox_-_The_Unreleased_v2.0.0_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/7kntfxf47vbh4cq/Incredibox_-_The_Unreleased_v2.0.0_Android.apk/file"
+    }
+  },
+  popzey: {
+    image: "img/icons/Popzey.png",
+    aliases: ['Pop Zey', 'The Unreleased'],
+    links: {
+        visit: "https://www.youtube.com/@PersonFromBrazil",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:The_Unreleased_V2",
+        desktop: "https://www.mediafire.com/file/lukkfuqfmcrnl2w/Incredibox_-_The_Unreleased_v2.0.0_PC.zip/file",
+        mobile: "https://www.mediafire.com/file/7kntfxf47vbh4cq/Incredibox_-_The_Unreleased_v2.0.0_Android.apk/file"
+    }
+  },
+  lovit: {
+    image: "img/icons/Lovit.webp",
+    links: {
+        visit: "https://www.youtube.com/@OxenLivia",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Lovit",
+        desktop: "https://www.mediafire.com/file/pa5wza7f901o436/Incredibox_-_Lovit.zip/file",
+        mobile: "https://www.mediafire.com/file/8q9ku624fxgx001/Incredibox_-_Lovit.apk/file"
+    }
+  },
+  otherside: {
+    image: "img/icons/Otherside.png",
+    aliases: ['Incrediblocks'],
+    links: {
+        visit: "https://www.youtube.com/@Siren_Bread",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Otherside",
+        desktop: "https://www.mediafire.com/file/tkrdwz9x52ouxrr/Incrediblocks_-_Aria_Math.zip/file",
+        mobile: "https://www.mediafire.com/file/q42vkwuz2x4xugk/Incrediblocks_-_Aria_Math_signed.apk/file"
+    }
+  },
+  aria: {
+    image: "img/icons/Aria.png",
+    aliases: ['Aria Math', 'Incrediblocks'],
+    links: {
+        visit: "https://www.youtube.com/@Siren_Bread",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Aria Math",
+        desktop: "https://www.mediafire.com/file/tkrdwz9x52ouxrr/Incrediblocks_-_Aria_Math.zip/file",
+        mobile: "https://www.mediafire.com/file/q42vkwuz2x4xugk/Incrediblocks_-_Aria_Math_signed.apk/file"
+    }
+  },
+  interstate: {
+    image: "img/icons/I190.webp",
+    aliases: ['Interstate 190'],
+    links: {
+        visit: "https://www.youtube.com/@overcookedlemonade",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Interstate 190",
+        desktop: "https://www.mediafire.com/file/tujy5e7j74uffp6/Incredibox_-_Interstate_190.zip/file",
+        mobile: "https://www.mediafire.com/file/2q0y4qe87qkvl4q/Interstate_190_Incredibox.apk/file"
+    }
+  },
+  runners: {
+    image: "img/icons/Runners.webp",
+    aliases: ['Sinebox'],
+    links: {
+        visit: "https://www.youtube.com/@sinetileofficial",
+        wiki: "https://incredibox.fandom.com/wiki/Fanon:Runners",
+        desktop: "https://www.mediafire.com/file/vkdb8ilmn63fynz/Sinebox+V1+-+Runners+1.5.0.zip/file",
+        mobile: "https://www.mediafire.com/file/virsofeny1k2sp9/Sinebox+-+Runners.apk/file"
+    }
+  }
+  };
